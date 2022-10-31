7fffb29c0c8130a678293638bb6ad6d7bf4cc72f
cpchristensen <112569319+cpchristensen615@users.noreply.github.com>
2022-10-31 02:01:19 +0000

    almost works

--- main.cpp
+++ main.cpp
@@ -1,27 +1,45 @@
 #include <iostream>
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
+void displayWinLose(bool winFlag);
+bool testWin(char wordArray[], char guessArray[], int wordchar, bool winFlag);
+
 int main() {
+  string wordString;
   char wordArray[16];
   char guessArray[26];
-  int correctCount;
   bool winFlag = false;
 
-  int wordchar = 7;
+  int wordchar;
+
+  cin >> wordString;
+
+  wordchar = wordString.length();
   
-  for(int i = 0; i < wordchar; i++)
-    cin >> wordArray[i];
+  strcpy(wordArray, wordString.c_str());
 
   for(int i = 0; i < 26; i++)
     cin >> guessArray[i];
 
-  
-  
-  for(int word = 0; word < wordchar; word++){ // for every letter in word
+  winFlag = testWin(wordArray, guessArray, wordchar, winFlag);
+  displayWinLose(winFlag);
+}
+
+// FUNCTIONS //
+void displayWinLose(bool winFlag){
+  if (winFlag == true)
+    cout << "WIN" << endl;
+  else if (winFlag == false)
+    cout << "LOSE" << endl;
+}
+
+bool testWin(char wordArray[], char guessArray[], int wordchar, bool winFlag){
+   for(int word = 0; word < wordchar; word++){ // for every letter in word
     winFlag = false;
-    for(int guess = 0; guess < wordchar+8; guess++){ // for alphabet through (word+10)
+    for(int guess = 0; guess < (wordchar+7); guess++){ // for alphabet through (word+10)
       if (wordArray[word] == guessArray[guess]){ // if letter matches guess
         winFlag = true; // make win true
       }
@@ -29,7 +47,5 @@ int main() {
     if (winFlag == false) // if there is no matching char
       break; // break loop and guess next letter
   }
-
-cout << winFlag;
-      
+  return winFlag;
 }
