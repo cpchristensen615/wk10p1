2746b1afbc395a99b92739b01744b5d3e3fe9839
cpchristensen <112569319+cpchristensen615@users.noreply.github.com>
2022-10-31 01:10:16 +0000

    works for 7 letter words

--- .ccls-cache/@home@runner@wk10p1/main.cpp
+++ .ccls-cache/@home@runner@wk10p1/main.cpp
@@ -16,14 +16,18 @@ int main() {
 
   for(int i = 0; i < 26; i++)
     cin >> guessArray[i];
+
   
-  for(int word = 0; word < wordchar; word++){
-    for(int guess = 0; guess < (wordchar+10); guess++) {
-      if (wordArray[word] == guessArray[guess])
-        winFlag = true;
+  
+  for(int word = 0; word < wordchar; word++){ // for every letter in word
+    winFlag = false;
+    for(int guess = 0; guess < wordchar+8; guess++){ // for alphabet through (word+10)
+      if (wordArray[word] == guessArray[guess]){ // if letter matches guess
+        winFlag = true; // make win true
+      }
     }
-    if (winFlag == false)
-      break;
+    if (winFlag == false) // if there is no matching char
+      break; // break loop and guess next letter
   }
 
 cout << winFlag;
